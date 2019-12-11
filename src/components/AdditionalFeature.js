4a81f49cc2340c9e016be782e93a7e0885d04506
thesoundfromthesky <nagara@naver.com>
2019-12-11 15:45:50 -0600

    add feature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { AddFeature } from "../store";
 
 const AdditionalFeature = props => {
+  const dispatch = useDispatch();
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(AddFeature(props.feature));
+        }}
+        className="button"
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
