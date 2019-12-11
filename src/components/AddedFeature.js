627d4e7d7aaccfab440477a65be7f64f0c094c62
thesoundfromthesky <nagara@naver.com>
2019-12-11 15:52:19 -0600

    remove dispatch

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+
+import { useDispatch } from "react-redux";
+import { RemoveFeature } from "../store";
 
 const AddedFeature = props => {
+  const dispatch = useDispatch();
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(RemoveFeature(props.feature));
+        }}
+        className="button"
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
